90c624c773f4f1202e175881f4765a012548b1b3
Daniel Fildán <fildand@centrum.cz>
2024-05-08 01:32:50 +0200

    Partially fix culling with upside down glasses

--- src/main/java/net/danygames2014/nyabsolutelyrandom/mixin/upsidedownglasses/class_555Mixin.java
+++ src/main/java/net/danygames2014/nyabsolutelyrandom/mixin/upsidedownglasses/class_555Mixin.java
@@ -17,10 +17,15 @@ public class class_555Mixin {
     public void invertScreen(CallbackInfo ci){
         Minecraft client = (Minecraft) FabricLoader.getInstance().getGameInstance();
         if(client.player != null && AccessoryAccess.hasAccessory(client.player, NyabsolutelyRandom.upsideDownGlasses)){
-            GL11.glDisable(GL11.GL_CULL_FACE);
+            GL11.glCullFace(GL11.GL_FRONT);
             GL11.glMatrixMode(GL11.GL_MODELVIEW);
             GL11.glLoadIdentity();
             GL11.glScalef(1.0F, -1.0F, 1.0F);
         }
+    }
+
+    @Inject(method = "method_1844", at = @At(value = "INVOKE", target = "Ljava/lang/System;nanoTime()J", ordinal = 1, shift = At.Shift.BEFORE))
+    public void revertCullFace(float par1, CallbackInfo ci){
+        GL11.glCullFace(GL11.GL_BACK);
     }
 }
